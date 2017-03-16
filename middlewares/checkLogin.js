1aaae73f9288c18bd6dd7fd9a23dc614bdaad7d4
张效伟 <zhmoll@zhmoll.com>
2017-03-17 01:00:26 +0800

    添加检测登录中间件，修改注销的代码

---
+++ middlewares/checkLogin.js
@@ -0,0 +1,33 @@
+var Token = require('../models/token');
+
+function checkLogin(req, res, next) {
+  var token = req.get('Authorization') || req.query.accesstoken;
+
+  if (!token) {
+    res.json({
+      type: 'error',
+      code: 42001,
+      message: '您尚未登录或登录凭证无效！'
+    });
+    return;
+  }
+
+  Token.findOne({ token: token }, function (err, token) {
+    if (err) return next(err);
+
+    if (!token) {
+      res.json({
+        type: 'error',
+        code: 42001,
+        message: '您尚未登录或登录凭证无效！'
+      });
+      return;
+    }
+    
+    res.locals.userid = token.userid;
+    res.locals.token = token.token;
+    return next();
+  });
+}
+
+module.exports = checkLogin;
--- routes/signout.js
+++ routes/signout.js
@@ -1,21 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Token = require('../models/token');
+var checkLogin = require('../middlewares/checkLogin');
 
-router.get('/', function (req, res, next) {
-  var token = req.get('Authorization');
-
-  Token.find({ token: token }, function (err, tokens) {
+router.get('/', checkLogin, function (req, res, next) {
+  Token.find({ token: res.locals.token }, function (err, tokens) {
     if (err) return next(err);
-
-    if (tokens.length == 0) {
-      res.json({
-        type: 'error',
-        code: 42001,
-        message: '登录凭据不存在或失效'
-      });
-      return;
-    }
 
     tokens.forEach(function (token) {
       token.remove();
